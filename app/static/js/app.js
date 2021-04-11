10981e1fb7f40ee41be636a2e9f215ed95e4dd97
Deron Campbell <deroncampbell88@gmail.com>
2021-04-11 13:12:50 -0500

    Complete lab6

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,107 @@
 /* Add your Application JavaScript */
+const Home = {
+  name: 'Home',
+  template: `
+    <div class="text-center">
+        <img src="/static/images/logo.png" alt="VueJS Logo">
+        <h1>{{ welcome }}</h1>
+    </div>
+  `,
+  data() {
+    return {
+      welcome: 'Hello World! Welcome to VueJS'
+    }
+  }
+};
+
+  const NewsList = {
+    name: 'NewsList',
+    template: `
+      <div class="news">
+          <h2 class="text-center news-heading">News</h2>   
+  
+          <div class="form-inline d-flex justify-content-center text-center search-area">
+            <div class="form-group mx-sm-3 mb-2">
+              <label class="sr-only" for="search">Search</label>        
+              <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+              <!--<p>You are searching for {{ searchTerm }}</p>-->
+              <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+            </div>
+          </div>
+  
+          <ul class="news__list">         
+            <li v-for="article in articles" class="news__item">
+              <h5>{{ article.title }}</h5>
+              <img :src="article.urlToImage" alt="" class="text-center"/>
+              <p>{{ article.description }}</p>
+            </li>
+          </ul>
+      </div>    
+    `,
+    created() {  
+      let self = this;
+
+      fetch('https://newsapi.org/v2/top-headlines?country=us', 
+      {    
+        headers: {
+          'Authorization': 'Bearer 66ea95e70ad2436583776ac3e36e739a'
+        }
+      })          
+      .then(function(response) {
+        return response.json();
+      })          
+      .then(function(data) {            
+        console.log(data);
+        self.articles = data.articles;
+      });
+    },
+    
+    data() {
+      return {
+        articles: [],
+        searchTerm: ''
+      }
+    },
+    methods: {
+      searchNews() {
+        let self = this;
+
+        fetch('https://newsapi.org/v2/everything?q='+ 
+        self.searchTerm + '&language=en', {
+          headers: {
+            'Authorization': 'Bearer 66ea95e70ad2436583776ac3e36e739a'
+          }
+        })
+
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(data);
+          self.articles = data.articles;
+        });
+      }
+    }
+  };
+  
+  const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes: [      
+      { path: '/', component: Home },      
+      { path: '/news', component: NewsList }  
+    ]
+  });
+
+
 const app = Vue.createApp({
   data() {
     return {
       welcome: 'Hello World! Welcome to VueJS'
     }
+  },
+  components: {
+    'home': Home,
+    'news-list': NewsList
   }
 });
 
@@ -12,18 +110,18 @@ app.component('app-header', {
   template: `
       <header>
           <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-            <a class="navbar-brand" href="#">VueJS App</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand">VueJS App</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarItems" aria-controls="navbarItems" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarItems">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <router-link to="/" class="nav-link">Home</router-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
+                  <router-link to="/news" class="nav-link">News</router-link>
                 </li>
               </ul>
             </div>
@@ -51,4 +149,5 @@ app.component('app-footer', {
   }
 })
 
+app.use(router)
 app.mount('#app');
